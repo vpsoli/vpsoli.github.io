364dded80dd67137bcee8d8b4199620607619e3c
Vinicius <viniciuspereirasilva@hotmail.com>
2019-07-27 15:08:32 -0300

    rubyBlog 2.0

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,38 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
+    @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
   end
   def show
     @article = Article.find(params[:id])
   end
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:text, :title))
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
   def create
     @article = Article.new(params.require(:article).permit(:text, :title))
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 end
